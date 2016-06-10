8c3975aa205397045271c337e9beb451086806a6
AmazingJaze <jessesh@microsoft.com>
2016-06-09 17:53:03 -0700

    updated project to work shared files. Enables WinJS SplitView and WinJS navigation

---
+++ Samples/Notifications/js/Notifications/js/sample-configuration.js
@@ -0,0 +1,20 @@
+﻿//// Copyright (c) Microsoft Corporation. All rights reserved
+
+(function () {
+    "use strict";
+
+    var sampleTitle = "Notifications JS sample";
+
+    var scenarios = [
+        { url: "/html/scenario1-livetiles.html", title: "Live tiles" },
+        { url: "/html/scenario2-badges.html", title: "Badges" },
+        //{ url: "/html/scenario3-enum.html", title: "Calendar enumeration and math" },
+        //{ url: "/html/scenario4-unicodeExtensions.html", title: "Calendar with Unicode extensions in languages" },
+        //{ url: "/html/scenario5-timeZone.html", title: "Calendar time zone support" }
+    ];
+
+    WinJS.Namespace.define("SdkSample", {
+        sampleTitle: sampleTitle,
+        scenarios: new WinJS.Binding.List(scenarios)
+    });
+})();
