1947a884e879e4e9f74bd2ad5debec16738b8fbc
AmazingJaze <jessesh@microsoft.com>
2016-06-24 16:19:38 -0700

    Adds listview with databinding to unpin everything scenario. Needs a little more work to get binding template to print. Will think about using Repeater instead of ListView.

--- Samples/Notifications/js/Notifications/js/scenario2-unpineverything.js
+++ Samples/Notifications/js/Notifications/js/scenario2-unpineverything.js
@@ -22,13 +22,63 @@
     // UI elements on the page
     var unpinCommand;
 
+    // UI controls on the page
+    var secondaryTilesListView;
+
     var page = WinJS.UI.Pages.define("/html/scenario2-unpineverything.html", {
         ready: function (element, options) {
+
             unpinCommand = element.querySelector("#unpincommand");
             unpinCommand.addEventListener("click", unpinTiles);
+
+            secondaryTilesListView = element.querySelector("#scenariocontrol").winControl;
+
+            getTilesData().then(function (data) {
+                debugger;
+                secondaryTilesListView.itemDataSource = new WinJS.Binding.List(data).dataSource;
+            })
         }
     });
 
+    function getTilesData() {
+        return new WinJS.Promise(c => {
+
+            SecondaryTile.findAllAsync().then(
+               function complete(tiles) {
+                   var data = tiles.map((tile) => {
+
+                       var bgColor = tile.visualElements.backgroundColor;
+                       var bgColorString = "{a: " + bgColor.a + ", b: " + bgColor.b + ", g: " + bgColor.g + ", r: " + bgColor.r + "}";
+
+                       return {
+                           displayName: tile.displayName,
+                           tileId: tile.tileId,
+                           arguments: tile.arguments,
+                           visualElements: {
+                               backgroundColor: bgColorString,
+                               foregroundText: tile.visualElements.foregroundText,
+                               showNameOnSquare150x150Logo: tile.visualElements.showNameOnSquare150x150Logo,
+                               showNameOnSquare310x310Logo: tile.visualElements.showNameOnSquare310x310Logo,
+                               showNameOnWide310x150Logo: tile.visualElements.showNameOnWide310x150Logo,
+                               square30x30Logo: tile.visualElements.square30x30Logo.displayUri,
+                               square44x44Logo: tile.visualElements.square44x44Logo.displayUri,
+                               square70x70Logo: tile.visualElements.square70x70Logo.displayUri,
+                               square71x71Logo: tile.visualElements.square71x71Logo.displayUri,
+                               square150x150Logo: tile.visualElements.square150x150Logo.displayUri,
+                               square310x310Logo: tile.visualElements.square310x310Logo.displayUri,
+                               wide310x150Logo: tile.visualElements.wide310x150Logo.displayUri,
+                           },
+                       }
+                   });
+
+                   c(data);
+               }),
+               function error(e) {
+                   debugger;
+               }
+
+        })
+    }
 
     function unpinTiles() {
         unpinCommand.disabled = true;
