44603f4508b7b80e2be089dd337d9a5c7a8042d5
AmazingJaze <jessesh@microsoft.com>
2016-06-28 22:42:01 -0700

    continued progress on tile notification scenario. Primary tile receives notification using old XML method

--- Samples/Notifications/js/Notifications/js/scenario3-primarytilenotifications.js
+++ Samples/Notifications/js/Notifications/js/scenario3-primarytilenotifications.js
@@ -3,6 +3,11 @@
 (function () {
     "use strict";
 
+    var XmlDocument = Windows.Data.Xml.Dom.XmlDocument;
+    var TileNotification = Windows.UI.Notifications.TileNotification;
+    var TileUpdateManager = Windows.UI.Notifications.TileUpdateManager;
+
+
     var SecondaryTile = Windows.UI.StartScreen.SecondaryTile;
     var TileSize = Windows.UI.StartScreen.TileSize;
     var Uri = Windows.Foundation.Uri;
@@ -18,67 +23,61 @@
         ready: function (element, options) {
 
             sendCommand = element.querySelector("#sendcommand");
-            sendCommand.addEventListener("click", pinTile);
+            sendCommand.addEventListener("click", sendNotification);
             sendCommand.disabled = false;
 
             clearCommand = element.querySelector("#clearcommand");
-            clearCommand.addEventListener("click", updateTile);
-            clearCommand.disabled = true;
+            //clearCommand.addEventListener("click", clearNotification);
+            //clearCommand.disabled = true;
 
-            restartCommand = element.querySelector("#restartcommand");
-            restartCommand.addEventListener("click", restartScenario);
+            //restartCommand = element.querySelector("#restartcommand");
+            //restartCommand.addEventListener("click", restartScenario);
 
         }
     });
 
-    function pinTile() {
+    function sendNotification() {
+        var xml = "" +
+            "<tile version='3'>" +
+                "<visual branding='nameAndLogo'>" +
+                    "<binding template='TileMedium'>" +
+                        "<text hint-wrap='true'>New tile notification</text>" +
+                        "<text hint-wrap='true' hint-style='captionSubtle'/>" +
+                    "</binding>" +
+                    "<binding template='TileWide'>" +
+                        "<text hint-wrap='true'>New tile notification</text>" +
+                        "<text hint-wrap='true' hint-style='captionSubtle'/>" +
+                    "</binding>" +
+                    "<binding template='TileLarge'>" +
+                        "<text hint-wrap='true'>New tile notification</text>" +
+                        "<text hint-wrap='true' hint-style='captionSubtle'/>" +
+                    "</binding>" +
+                "</visual>" +
+                "</tile>";        
 
-        sendCommand.disabled = true;
+        var doc = new XmlDocument();
+        doc.loadXml(xml);
 
-        // Generate a unique tile ID for the purposes of the sample
-        _tileId = new Date().getTime().toString();
+        var nowTimeString = new Date().getTime().toString();
+        var nodes = doc.selectNodes("//text");
 
-        // Initialize and pin a new secondary tile that we will later update
-        var tile = new SecondaryTile(_tileId, "Original", "args", new Uri("ms-appx:///images/cancel.png"), TileSize.default);
-        tile.visualElements.square71x71Logo = new Uri("ms-appx:///images/cancel.png");
-        tile.visualElements.wide310x150Logo = new Uri("ms-appx:///images/cancel.png");
-        tile.visualElements.square310x310Logo = new Uri("ms-appx:///images/cancel.png");
-        tile.visualElements.showNameOnSquare150x150Logo = true;
-        tile.visualElements.showNameOnSquare310x310Logo = true;
-        tile.visualElements.showNameOnWide310x150Logo = true;
-
-        tile.requestCreateAsync().then(function complete() {
-            // unlock the rest of the scenario
-            clearCommand.disabled = false;
-        });
-    }
-
-    function updateTile() {
-        clearCommand.disabled = true;
-
-        // Grab the existing tile
-        SecondaryTile.findAllAsync().then(function complete(tiles) {
-
-            var tile = tiles.filter(function (tile) {
-                return tile.tileId === _tileId;
-            })[0];
-
-            if (!tile) {
-                new MessageDialog("The secondary tile that was previously pinned could not be found. Has it been removed from Start?", "Error").showAsync();
-            } else {
-
-                // Change its name and logo
-                tile.displayName = "Updated";
-                tile.visualElements.square150x150Logo = new Uri("ms-appx:///images/check.png");
-                tile.visualElements.square71x71Logo = new Uri("ms-appx:///images/check.png");
-                tile.visualElements.square310x310Logo = new Uri("ms-appx:///images/check.png");
-                tile.visualElements.wide310x150Logo = new Uri("ms-appx:///images/check.png");
-
-                // And request its properties to be updated
-                tile.updateAsync();
+        // Assign date/time values through XmlDocument to avoid any xml escaping issues
+        nodes.forEach(function (textEl) {
+            if (textEl.innerText.length == 0) {
+                textEl.innerText = nowTimeString;
             }
 
+            var notification = new TileNotification(doc);
+            TileUpdateManager.createTileUpdaterForApplication().update(notification);
         });
+
+        // Assign date/time values through XmlDocument to avoid any xml escaping issues
+        //foreach (XmlElement textEl in doc.SelectNodes("//text").OfType<XmlElement>())
+        //if (textEl.InnerText.Length == 0)
+        //    textEl.InnerText = nowTimeString;
+
+        //TileNotification notification = new TileNotification(doc);
+        //TileUpdateManager.CreateTileUpdaterForApplication().Update(notification);
     }
 
     function restartScenario() {
