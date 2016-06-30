394b3cb99703fee3740c2f23a4ceae3ab23d43a7
AmazingJaze <jessesh@microsoft.com>
2016-06-29 22:20:01 -0700

    Adds NotificationsExtensions Nuget package, and uses it to replaces XML string for tile notifications

--- Samples/Notifications/js/Notifications/js/scenario3-primarytilenotifications.js
+++ Samples/Notifications/js/Notifications/js/scenario3-primarytilenotifications.js
@@ -6,6 +6,7 @@
     var XmlDocument = Windows.Data.Xml.Dom.XmlDocument;
     var TileNotification = Windows.UI.Notifications.TileNotification;
     var TileUpdateManager = Windows.UI.Notifications.TileUpdateManager;
+    var Tiles = NotificationsExtensions.Tiles;
 
 
     var SecondaryTile = Windows.UI.StartScreen.SecondaryTile;
@@ -35,40 +36,75 @@
         }
     });
 
-    function sendNotification() {
-        var xml = "" +
-            "<tile version='3'>" +
-                "<visual branding='nameAndLogo'>" +
-                    "<binding template='TileMedium'>" +
-                        "<text hint-wrap='true'>New tile notification</text>" +
-                        "<text hint-wrap='true' hint-style='captionSubtle'/>" +
-                    "</binding>" +
-                    "<binding template='TileWide'>" +
-                        "<text hint-wrap='true'>New tile notification</text>" +
-                        "<text hint-wrap='true' hint-style='captionSubtle'/>" +
-                    "</binding>" +
-                    "<binding template='TileLarge'>" +
-                        "<text hint-wrap='true'>New tile notification</text>" +
-                        "<text hint-wrap='true' hint-style='captionSubtle'/>" +
-                    "</binding>" +
-                "</visual>" +
-                "</tile>";
+    //function sendNotification() {
+    //    var xml = "" +
+    //        "<tile version='3'>" +
+    //            "<visual branding='nameAndLogo'>" +
+    //                "<binding template='TileMedium'>" +
+    //                    "<text hint-wrap='true'>New tile notification</text>" +
+    //                    "<text hint-wrap='true' hint-style='captionSubtle'/>" +
+    //                "</binding>" +
+    //                "<binding template='TileWide'>" +
+    //                    "<text hint-wrap='true'>New tile notification</text>" +
+    //                    "<text hint-wrap='true' hint-style='captionSubtle'/>" +
+    //                "</binding>" +
+    //                "<binding template='TileLarge'>" +
+    //                    "<text hint-wrap='true'>New tile notification</text>" +
+    //                    "<text hint-wrap='true' hint-style='captionSubtle'/>" +
+    //                "</binding>" +
+    //            "</visual>" +
+    //            "</tile>";
 
-        var doc = new XmlDocument();
-        doc.loadXml(xml);
+    //    var doc = new XmlDocument();
+    //    doc.loadXml(xml);
+
+    //    var nowTimeString = new Date().getTime().toString();
+    //    var nodes = doc.selectNodes("//text");
+
+    //    // Assign date/time values through XmlDocument to avoid any xml escaping issues
+    //    nodes.forEach(function (textEl) {
+    //        if (textEl.innerText.length == 0) {
+    //            textEl.innerText = nowTimeString;
+    //        }
+    //    });
+
+    //    var notification = new TileNotification(doc);
+    //    TileUpdateManager.createTileUpdaterForApplication().update(notification);
+    //}
+
+    function sendNotification() {
 
         var nowTimeString = new Date().getTime().toString();
-        var nodes = doc.selectNodes("//text");
 
-        // Assign date/time values through XmlDocument to avoid any xml escaping issues
-        nodes.forEach(function (textEl) {
-            if (textEl.innerText.length == 0) {
-                textEl.innerText = nowTimeString;
+        var textConfigs = [
+            { text: "New tile notification", wrap: true },
+            { text: nowTimeString, wrap: true, style: Tiles.TileTextStyle.captionSubtle }
+        ];
+
+        // Build and append content from textConfigs, line by line.
+        var adaptiveContent = new Tiles.TileBindingContentAdaptive();
+        textConfigs.forEach(lineConfig => {
+            var lineOfTileText = new Tiles.TileText();
+            for (var key in lineConfig) {
+                lineOfTileText[key] = lineConfig[key];
             }
-
-            var notification = new TileNotification(doc);
-            TileUpdateManager.createTileUpdaterForApplication().update(notification);
+            adaptiveContent.children.push(lineOfTileText);
         });
+
+        // Specify templates and send Notification.
+        var tileContent = new Tiles.TileContent();
+        tileContent.visual = new Tiles.TileVisual();
+        tileContent.visual.branding = Tiles.TileBranding.nameAndLogo;
+        tileContent.visual.tileMedium = new Tiles.TileBinding();
+        tileContent.visual.tileMedium.content = adaptiveContent;
+        tileContent.visual.tileWide = new Tiles.TileBinding();
+        tileContent.visual.tileWide.content = adaptiveContent;
+        tileContent.visual.tileLarge = new Tiles.TileBinding();
+        tileContent.visual.tileLarge.content = adaptiveContent;
+
+        var doc = tileContent.getXml();
+        var notification = new TileNotification(doc);
+        TileUpdateManager.createTileUpdaterForApplication().update(notification);
     }
 
     function clearNotification() {
